5c78a9a056b5151cad88d79f8724f59dc691a432
alecha98 <ale_alexandra@mail.ru>
2015-12-08 09:39:01 +0300

    Update hero.py

--- hero.py
+++ hero.py
@@ -6,5 +6,22 @@ from gameunit import *
 """В этом файле должен быть описан класс героя, унаследованный от Attacker
 Герой должен иметь 100 поинтов здоровья, атаку 50, опыт 0, имя, задаваемое в конструкторе
 Метод attack должен получать атрибут target и уменьшать его здоровье на величину атаки.
-
 """
+
+class Hero(Attacker):
+    _name = None
+
+    def __init__(self, name):
+        self._name = name
+        self._health = 100
+        self._attack = 50
+        self._experience = 0
+
+    def attack(self, target):
+        target._health -= self._attack
+
+    def experience (self):
+        self._experience += self._attack
+
+    def gameOver (self):
+        pass
